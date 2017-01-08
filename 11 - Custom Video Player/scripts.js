84ef5972acc08fca25f0f906f82756de257c2ca7
mjdude <mo@syncspot.net>
2017-01-08 23:35:52 +0000

    added toggleplay

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,8 +2,20 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progerssBar = progress.querySelector('.progress__filled');
+const progerssBar = player.querySelector('.progress__filled');    
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
 
 // Build functions
 
+function togglePlay(){
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 // Hook up event listeners
+toggle.addEventListener('click', togglePlay);
