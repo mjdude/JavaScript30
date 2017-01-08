ad591794a4379af3dedc64ddba36f7cddac35692
mjdude <mo@syncspot.net>
2017-01-08 22:58:58 +0000

    starting to style progress bar

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,9 @@
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progerssBar = progress.querySelector('.progress__filled');
+
+// Build functions
+
+// Hook up event listeners
