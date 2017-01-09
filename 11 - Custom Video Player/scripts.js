5369c0167d21bfe155edacafe8604367317335cd
mjdude <mo@syncspot.net>
2017-01-09 22:48:55 +0000

    added playback rate and volume event listener

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progerssBar = player.querySelector('.progress__filled');    
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 
 // Build functions
@@ -19,9 +19,26 @@ function togglePlay(){
 }
 
 function updateButton(){
-    const icon = this.paused ? '►' : '▌ ▐';
+    const icon = this.paused ? '►' : '▌▐';
     toggle.textContent = icon;
     console.log('Update the button');
+}
+
+function skip(){
+    console.log(this.dataset);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    console.log(this.name, this.value);
+    if (this.name === 'volume') {
+        video.volume = this.value;    
+    };
+    
+    if (this.name === 'playbackRate') {
+        video.playbackRate = this.value   
+    };
+
 }
 
 // Hook up event listeners
@@ -31,3 +48,11 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach((button) => {
+    button.addEventListener('click', skip);
+});
+
+ranges.forEach((range) => {
+    range.addEventListener('click', handleRangeUpdate);
+});
